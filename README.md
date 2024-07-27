79ec8857101b09f77d4e1da744c21254b3e4b726
rallmac2 <root@tobijahdevice>
2024-07-27 20:25:03 +0100

    Reviewing the simple_shell project

---
+++ README.md
@@ -0,0 +1,1 @@
+I am glad to have my personal files repo. Now I can save my work, reveiw and improve them
---
+++ simple_shell/main.c
@@ -0,0 +1,69 @@
+#include "shell.h"
+
+/**
+ * free_data - Frees the memory allocated for the data_shell structure.
+ * @datash: Pointer to the data_shell structure to be freed.
+ * Return: void
+ */
+void free_data(data_shell *datash)
+{
+	unsigned int i;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initializes the data_shell structure with provided arguments and environment variables.
+ * @datash: Pointer to the data_shell structure to be initialized.
+ * @av: Array of strings representing command-line arguments.
+ * Return: void
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point of the shell program.
+ * @ac: Number of command-line arguments.
+ * @av: Array of strings representing command-line arguments.
+ * Return: Integer representing the exit status of the shell program.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
